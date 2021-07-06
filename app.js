aff97cb75dfb6f5687b9a4d6e813547e4a3b1660
andre.dealbuquerque@modis.com <andre.dealbuquerque@modis.com>
2021-07-06 21:23:48 +0200

    First class

---
+++ app.js
@@ -0,0 +1,80 @@
+console.log("Hello Remote Potatoes");
+
+// Variable types?
+// No: types in JAVASCRIPT
+// Strings, Numbers, Boolean, Array, Undefined, Null, Function, Object, NaN, Symbol, BigInt
+
+const variable = 1; // variable declaration, variable assignment
+let variableOfANumber; // variable declaration
+
+variableOfANumber = 1; // variable assignment
+
+variableOfANumber = 2; // variable reassignment
+
+// Whenever we code, we code for the computer, and for other developers
+console.log(variable);
+console.log(variableOfANumber);
+variableOfANumber = 3;
+console.log(variableOfANumber);
+
+let nameOfStudent = "Noemi";
+
+nameOfStudent[4] = "L";
+// nameOfStudent = "Juan";
+
+console.log(nameOfStudent);
+
+// keyword variablenme = value
+
+let sum = 1 + 1;
+console.log(sum);
+
+console.log(1 - 1);
+
+console.log(7 * 7);
+
+console.log(7 / 7);
+
+console.log(10 % 3);
+console.clear();
+let numberExample = 0;
+console.log("numberExample:", numberExample);
+numberExample++;
+numberExample++;
+numberExample++;
+numberExample++;
+numberExample--;
+numberExample--;
+numberExample--;
+numberExample--;
+numberExample = numberExample + 5;
+numberExample += 5;
+numberExample -= 5;
+numberExample *= 5;
+numberExample /= 5;
+numberExample **= 5;
+
+console.log("numberExample:", numberExample);
+// numberExample = numberExample + 1
+
+let anotherStudentName = "Anna";
+anotherStudentName += " Velarde";
+console.log("anotherStudentName:", anotherStudentName);
+console.log(anotherStudentName - " Velarde");
+
+console.log("1" + 1, typeof ("1" + 1));
+console.log(1 + "1", typeof (1 + "1"));
+
+("hello");
+("hello");
+`hello`;
+
+const theSecondPart = "there";
+console.log("hello" + " " + theSecondPart);
+console.log(`hello ${theSecondPart}`);
+console.log(theSecondPart.length);
+console.log("donde esta la biblioteca".toUpperCase());
+
+let string = "Daniel tem Damme";
+
+console.log(string.split("e"));
